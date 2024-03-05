318fb640663f20e2fb0be5d14fa8cf76022b0902
Jesper276 <jespermadsenvejle@hotmail.dk>
2024-03-05 11:28:14 +0100

    added start time and end time

--- src/Extracting_data.ts
+++ src/Extracting_data.ts
@@ -1,20 +1,22 @@
 import sql from './db.js'
 export class LocationDatabase {
-    async GetLocations(): Promise<{ imsi: number, x: number, y: number, calctime: number }> {
-        const Locations = await sql`
-        SELECT
-            imsi,
-            x,
-            y,
-            calctime
-        FROM
-            Location`;
-
-        return Locations as any
-    }
-
-    async GetMeasurements(): Promise<{ mid: number, imsi: number, aid: number, timestamp: number, strengthDBM: number }> {
-        const Measurements = await sql`
+    async GetLocations(startTime?: number, endTime?: number): Promise<{ imsi: number, x: number, y: number, calctime: number }> {
+        if (startTime && endTime) {
+            const Locations = await sql`
+            SELECT
+                imsi,
+                x,
+                y,
+                calctime
+            FROM
+                Location
+            WHERE
+                timestamp <= ${startTime}
+                AND timestamp >= ${endTime}
+                `
+            return Locations as any
+        } else {
+            const Measurements = await sql`
             SELECT 
                 mid,
                 imsi,
@@ -22,9 +24,42 @@ export class LocationDatabase {
                 timestamp,
                 strengthDBM
             FROM
-                Measurement`;
+                Measurement 
+            `
+            return Measurements as any
+        }
 
-        return Measurements as any
+    }
+
+    async GetMeasurements(startTime?: number, endTime?: number): Promise<{ mid: number, imsi: number, aid: number, timestamp: number, strengthDBM: number }> {
+        if (startTime && endTime) {
+            const Measurements = await sql`
+            SELECT 
+                mid,
+                imsi,
+                aid,
+                timestamp,
+                strengthDBM
+            FROM
+                Measurement
+                WHERE
+                timestamp <= ${startTime}
+            AND timestamp >= ${endTime}`
+
+            return Measurements as any
+        } else {
+            const Measurements = await sql`
+            SELECT 
+                mid,
+                imsi,
+                aid,
+                timestamp,
+                strengthDBM
+            FROM
+                Measurement 
+            `
+            return Measurements as any
+        }
     }
 
     async InsertCalculations(imsi: number, calctime: number, mid: number) {
