7432fc78af7eff69a7abacf0c1d3679036c53a10
Jesper276 <90216888+Jesper276@users.noreply.github.com>
2024-03-07 13:58:24 +0100

    made some quries

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
-import { AppDataSource } from "./data-source.ts"
-import { Prisma, PrismaClient } from '@prisma/client'
-import { setupDB } from "./setup_db.ts";
+import { AppDataSource } from "./data-source.ts";
+import { Prisma, PrismaClient } from "@prisma/client";
 import { Query } from "typeorm/driver/Query.js";
-import { measureMemory } from "vm";
 
 export class LocationDatabase {
     private Prisma: PrismaClient;
 
     constructor() {
-        this.Prisma = new PrismaClient;
+        this.Prisma = new PrismaClient();
     }
 
-    async GetLocations(startTime?: number, endTime?: number): Promise<{
-        imsi: bigint;
-        x: number | null;
-        y: number | null;
-        calctime: bigint;
-    }[]> {
+    async GetLocations(
+        startTime?: number,
+        endTime?: number
+    ): Promise<
+        {
+            imsi: bigint;
+            x: number | null;
+            y: number | null;
+            calctime: bigint;
+        }[]
+    > {
         let query: Prisma.locationFindManyArgs = {};
 
         if (startTime && endTime) {
@@ -31,14 +34,22 @@ export class LocationDatabase {
         const locations = await this.Prisma.location.findMany(query);
         return locations;
     }
-    async GetMeasurements(startTime?: number, endTime?: number): Promise<{
-        mid: number,
-        imsi: number,
-        aid: number,
-        timestamp: number,
-        strengthDBM: number
-    }[]> {
+
+    async GetMeasurements(
+        startTime?: number,
+        endTime?: number
+    ): Promise<
+        {
+            mid: number;
+            imsi: bigint;
+            aid: number;
+            timestamp: bigint;
+            strengthDBM: number;
+        }[]
+    > {
         let query: Prisma.measurementFindFirstArgs = {};
+
+        //if there is no startTime and endTime then it returns all the measurements
         if (startTime && endTime) {
             query.where = {
                 timestamp: {
@@ -49,6 +60,41 @@ export class LocationDatabase {
         }
 
         const Measurement = await this.Prisma.measurement.findMany(query);
-        return Measurement
+        return Measurement;
     }
 
+    async InsertCalculations(imsi: number, calctime: number, mid: number) {
+        const insertetData = await this.Prisma.calculation.create({
+            data: {
+                imsi: imsi,
+                calctime: calctime,
+                mid: mid,
+            },
+        });
+    }
+
+    async InsertLocations(
+        imsi: number,
+        calctime: number,
+        x: number,
+        y: number
+    ) {
+        const InsertData = await this.Prisma.location.create({
+            data: {
+                imsi: imsi,
+                calctime: calctime,
+                x: x,
+                y: y,
+            },
+        });
+    }
+
+    async InsertAntenna(x: number, y: number) {
+        const insertData = await this.Prisma.antennas.create({
+            data: {
+                x: x,
+                y: y,
+            },
+        });
+    }
+}
