d8db74b4dbbd3d1bed0cef1b33d810a2640551f4
AlbertHald <43075950+AlbertHald@users.noreply.github.com>
2024-03-13 14:36:16 +0100

    ts types + shell script

--- src/gRPC/gRPC-server.ts
+++ src/gRPC/gRPC-server.ts
@@ -1,72 +1,47 @@
 import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
-import { get } from 'http';
+import { ProtoGrpcType } from '../../build/protobuf/route_guide';
+import { RoutesHandlers } from '../../build/protobuf/Routes';
+import { Empty__Output, Empty } from '../../build/protobuf/Empty';
+import { GetAntennasResponse } from '../../build/protobuf/GetAntennasResponse';
+import { GetLocationsRequest__Output } from '../../build/protobuf/GetLocationsRequest';
+import { GetLocationsResponse } from '../../build/protobuf/GetLocationsResponse';
+import { LocationMeasurementsRequest__Output } from '../../build/protobuf/LocationMeasurementsRequest';
+import { LocationMeasurementsResponse } from '../../build/protobuf/LocationMeasurementsResponse';
+import { LogMeasurementRequest__Output } from '../../build/protobuf/LogMeasurementRequest';
+import { RegisterAntennaRequest__Output } from '../../build/protobuf/RegisterAntennaRequest';
+import { RegisterAntennaResponse } from '../../build/protobuf/RegisterAntennaResponse';
 
-const PROTO_PATH = './proto/cdm_protobuffer.proto';
+const PROTO_PATH = '../../CDM-ProtocolBuffer/route_guide.proto';
 
 //Load the protocol buffer 
-const packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {
-        keepCase: true,
-        longs: String,
-        enums: String,
-        defaults: true,
-        oneofs: true
-    });
-const cdm_protobuffer: any = grpc.loadPackageDefinition(packageDefinition).cdm_protobuffer;
+const packageDefinition = protoLoader.loadSync(PROTO_PATH);
+const cdm_protobuffer = grpc.loadPackageDefinition(packageDefinition) as unknown as ProtoGrpcType;
 
+const routeHandlers : RoutesHandlers = {
+    GetAntennasRoute: function (call: grpc.ServerUnaryCall<Empty__Output, GetAntennasResponse>, callback: grpc.sendUnaryData<GetAntennasResponse>): void {
+        throw new Error('Function not implemented.');
+    },
+    GetLocationMeasurementsRoute: function (call: grpc.ServerUnaryCall<LocationMeasurementsRequest__Output, LocationMeasurementsResponse>, callback: grpc.sendUnaryData<LocationMeasurementsResponse>): void {
+        throw new Error('Function not implemented.');
+    },
+    GetLocationsRoute: function (call: grpc.ServerUnaryCall<GetLocationsRequest__Output, GetLocationsResponse>, callback: grpc.sendUnaryData<GetLocationsResponse>): void {
+        throw new Error('Function not implemented.');
+    },
+    LogMeasurementRoute: function (call: grpc.ServerReadableStream<LogMeasurementRequest__Output, Empty>, callback: grpc.sendUnaryData<Empty>): void {
+        throw new Error('Function not implemented.');
+    },
+
+    //Registers a new antenna
+    RegisterAntennaRoute: function (call: grpc.ServerUnaryCall<RegisterAntennaRequest__Output, RegisterAntennaResponse>, callback: grpc.sendUnaryData<RegisterAntennaResponse>): void {
+        throw new Error('Function not implemented.');
+    }
+}
 //Set up gRPC server with all services
 export function getServer() : grpc.Server {
     const server = new grpc.Server();
 
-    server.addService(cdm_protobuffer.Routes.service, {
-        LogMeasurementRoute: logMeasurementRoute,
-        RegisterAntennaRoute: registerAntennaRoute,
-        GetAntennasRoute: getAntennasRoute,
-        GetLocationsRoute : getLocationsRoute,
-        GetLocationMeasurementsRoute : getLocationMeasurementsRoute
-    });
+    server.addService(cdm_protobuffer.Routes.service, routeHandlers);
     
     return server;
 };
-//Start the server TODO
-
-/**
- * The implementations of the services from the protocol buffer
- * @param call, the call from the client
- * @param callback, the callback to the client
- * @returns void
- */
-//TODO find types for call and callback
-function logMeasurementRoute(call: grpc.ServerReadableStream<any, any>, callback: grpc.sendUnaryData<object>) : void {
-    //Everytime log-data comes in, it will be logged in the database
-    let logcounter : number = 0;
-    call.on('data', (data) => {
-
-    });
-
-    //Sends "empty" response to the client
-    call.on('end', () => {
-        callback(null, { message: 'Measurements logged' });
-    });
-}
-
-function registerAntennaRoute(call: grpc.ServerUnaryCall<any, any>, callback: grpc.sendUnaryData<object>) : void {
-    let antenna = call.request;
-    callback(antenna, /*registerAntenna(antenna)*/);
-}
-
-function getAntennasRoute(call: grpc.ServerUnaryCall<any, any>, callback: grpc.sendUnaryData<any>) : void {
-    //Implementation needed
-    getAntennas(call.request);
-    callback(null, /*getAntennas(call.request)*/);
-}
-
-function getLocationsRoute(call: grpc.ServerUnaryCall<any, any>, callback: grpc.sendUnaryData<any>) : void {
-    //Implementation needed
-}
-
-function getLocationMeasurementsRoute(call: grpc.ServerUnaryCall<any, any>, callback: grpc.sendUnaryData<any>) : void {
-    //Implementation needed
-}
