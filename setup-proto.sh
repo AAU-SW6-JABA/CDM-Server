78abb324aca9614c6441dcb9446b832ed74d5102
Anton <dev@atjn.dk>
2024-03-20 13:24:35 +0100

    Streamline gRPC type generation

--- setup-proto.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir -p build/protobuf
-yarn
-#For use with dynamic code generation
-yarn run proto-loader-gen-types --grpcLib=@grpc/grpc-js --outDir=build/protobuf CDM-ProtocolBuffer/cdm_protobuf.proto
