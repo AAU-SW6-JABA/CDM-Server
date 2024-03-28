0efed816e6e78a779c317ae4fd577e0497b39159
Jesper276 <jespermadsenvejle@hotmail.dk>
2024-03-28 09:54:54 +0100

    added test data

--- Triangulation/LSM.ts
+++ Triangulation/LSM.ts
@@ -10,15 +10,15 @@ export function GetXAndY(antennas: Antenna[]): {
 
 	// Calculate squared distances to each antenna
 	const AntennasSquared = antennas.map(
-		(antenna) => antenna.x * antenna.x + antenna.y * antenna.y,
+		(antenna) => antenna.x ** 2 + antenna.y ** 2,
 	);
 
 	// Calculate squared distance differences
 	const squaredDistanceDifferences = [];
 	for (let i = 1; i < antennas.length; i++) {
 		const squaredDifference =
-			antennas[i].distance * antennas[i].distance -
-			antennas[i - 1].distance * antennas[i - 1].distance -
+			antennas[i].distance ** 2 -
+			antennas[i - 1].distance ** 2 -
 			AntennasSquared[i] +
 			AntennasSquared[i - 1];
 		squaredDistanceDifferences.push(squaredDifference);
@@ -43,44 +43,17 @@ export function GetXAndY(antennas: Antenna[]): {
 		const antenna2 = antennas[i];
 		const antenna3 = antennas[i + 1];
 		x +=
-			(squaredDistanceDifferences[i - 1] * (antenna3.y - antenna2.y) -
+			((squaredDistanceDifferences[i - 1] * (antenna3.y - antenna2.y) -
 				squaredDistanceDifferences[i] * (antenna2.y - antenna1.y)) /
-			denominator;
+				denominator) *
+			-1;
 		y +=
-			(squaredDistanceDifferences[i - 1] * (antenna2.x - antenna3.x) -
+			((squaredDistanceDifferences[i - 1] * (antenna2.x - antenna3.x) -
 				squaredDistanceDifferences[i] * (antenna1.x - antenna2.x)) /
-			denominator;
+				denominator) *
+			-1;
 	}
 
 	// Return the calculated coordinates
 	return { x, y };
 }
-// 	let Antenna1Squared = Antenna1.x * Antenna1.x + Antenna1.y * Antenna1.y;
-// 	let Antenna2Squared = Antenna2.x * Antenna2.x + Antenna2.y * Antenna2.y;
-// 	let Antenna3Squared = Antenna3.x * Antenna3.x + Antenna3.y * Antenna3.y;
-
-// 	//this uses squared distances to the antennas and thier know postions.
-// 	const a =
-// 		distanceToAntenna1 * distanceToAntenna1 -
-// 		distanceToAntenna2 * distanceToAntenna2 -
-// 		Antenna1Squared +
-// 		Antenna2Squared;
-// 	const b =
-// 		distanceToAntenna2 * distanceToAntenna2 -
-// 		distanceToAntenna3 * distanceToAntenna3 -
-// 		Antenna2Squared +
-// 		Antenna3Squared;
-
-// 	// Calculate the denominator
-// 	const c =
-// 		2 * (Antenna2.x - Antenna1.x) * (Antenna3.y - Antenna2.y) -
-// 		2 * (Antenna3.x - Antenna2.x) * (Antenna2.y - Antenna1.y);
-
-// 	// Calculate the x and y coordinates
-// 	const x =
-// 		(a * (Antenna3.y - Antenna2.y) - b * (Antenna2.y - Antenna1.y)) / c;
-// 	const y =
-// 		(a * (Antenna2.x - Antenna3.x) - b * (Antenna1.x - Antenna2.x)) / c;
-
-// 	return { x, y };
-//
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 import * as grpc from "@grpc/grpc-js";
 import { GRPCServer } from "./gRPC/GRPCServer.ts";
 import dotenv from "dotenv";
+import { GetXAndY } from "../Triangulation/LSM.ts";
+import { Antenna } from "../Triangulation/Antenna.ts";
 
-dotenv.config();
-//Start the gRPC server
-const server = new GRPCServer(
-	"CDM-ProtocolBuffer/cdm_protobuf.proto",
-).getServer();
-server.bindAsync(
-	`localhost:${process.env.PORT}`,
-	grpc.ServerCredentials.createInsecure(),
-	() => {
-		server;
-		console.log(`Server running at http://localhost:${process.env.PORT}`);
-	},
-);
+let Antenna1: Antenna = { x: 0, y: 0, distance: 3.16 };
+let Antenna2: Antenna = { x: 3, y: 2, distance: 1 };
+let Antenna3: Antenna = { x: 5, y: 0, distance: 2.24 };
+let Antenna4: Antenna = { x: 1, y: 2, distance: 2.24 };
+let Antenna5: Antenna = { x: 5, y: 2, distance: 2.24 };
+let Antenna6: Antenna = { x: 4.03, y: 1.33, distance: 1.09 };
+let Antenna7: Antenna = { x: 3, y: 0.3, distance: 0.7 };
+let Antenna8: Antenna = { x: 1.84, y: 1.14, distance: 1.16 };
+
+let AllAntennas: Antenna[] = [Antenna4, Antenna5, Antenna6, Antenna7, Antenna8];
+
+let Antennas: Antenna[] = [Antenna1, Antenna2, Antenna3];
+
+for (const antenna of AllAntennas) {
+	let { x: test1x, y: test1y } = GetXAndY(Antennas);
+	console.log(GetXAndY(Antennas));
+	console.log((test1x / 3) * 100 + "%", (test1y / 1) * 100 + "%");
+	Antennas.push(antenna);
+}
